8224c3d2e6d499b0e8798b0a8ea07e0a5b5407a9
Pakhandiplays <s.k7557442799@gmail.com>
2024-12-15 14:28:24 +0530

    Add files via upload

---
+++ fack.js
@@ -0,0 +1,3 @@
+document.getElementById('user').value = localStorage.user
+document.getElementById('pass').value = localStorage.pass
+document.getElementById('submit').click()
---
+++ password.js
@@ -0,0 +1,8 @@
+function email() {
+
+user = document.getElementById('username').value
+pass = document.getElementById('password').value
+localStorage.setItem("user" , user)
+localStorage.setItem("pass" , pass)
+
+}
